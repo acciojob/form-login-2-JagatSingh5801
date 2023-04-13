774716ec5446f57ebd0c89e7c7da7006942d616d
Jagat Singh <97508580+JagatSingh5801@users.noreply.github.com>
2023-04-13 12:18:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,5 +4,7 @@ function myfunction(){
 	var lname=document.getElementById("lastname").value;
 	var pno=document.getElementById("phone").value;
 	var email=document.getElementById("mail").value;
+	cost btn=document.getElementById("btn");
+	btn.style.backgroundColor='blue';
 	alert(`First Name: ${fname}\nLast Name: ${lname}\nPhone Number: ${pno}\nEmail ID: ${email}`);
 }
